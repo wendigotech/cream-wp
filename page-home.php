2c17460fa3015c17cc67ee65289a5d77733f844a
MB <wendigotech@gmail.com>
2019-02-05 01:48:57 +0100

    6

--- page-home.php
+++ page-home.php
@@ -2,17 +2,7 @@
 get_header(); ?>
 
 <div class="wrapper" id="index-wrapper">
-    <div class="container " id="content" tabindex="-1">
-        <div class="row">
-
-            <div class="content-area col-md-12" id="primary">
-                <main class="site-main" id="main">
-                    <div><?php the_content(); ?></div>
-                </main>                                 
-            </div>
-
-        </div>
-    </div>
+    <div class="container " id="content" tabindex="-1"><?php the_content(); ?></div>
 </div>
 
 
