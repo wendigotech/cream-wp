4f0d2bd4c07c6ad4718389bceef1d26d03033f87
MB <wendigotech@gmail.com>
2019-03-05 11:06:06 +0100

    4

--- loop-templates/content.php
+++ loop-templates/content.php
@@ -4,9 +4,9 @@
             <article <?php post_class( 'mb-5' ); ?> id="post-<?php the_ID(); ?>"> 
                 <header class="entry-header"> 
                     <?php if ( is_singular() ) : ?>
-                        <h2><?php the_title(); ?></h2>
+                        <h2 class="text-center"><?php the_title(); ?></h2>
                     <?php else : ?>
-                        <h2><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a></h2>
+                        <h2 class="text-center"><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a></h2>
                     <?php endif; ?> 
                     <div class="entry-meta"> 
                         <p><?php _e( 'Posted on', 'cream' ); ?> <span><?php the_modified_date(); ?></span> <?php _e( 'by', 'cream' ); ?> <span><a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>"><?php the_author(); ?></a></span></p> 
